a80dee048886d8c8b38643ee7e1aa9aca12409ff
Evgeny <starchenkogin@gmail.com>
2021-04-06 00:19:29 +0300

    login

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,6 +1,8 @@
 const Router = require("express")
 const User = require("../models/User")
 const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const config = require("config")
 const  { check, validationResult } = require("express-validator")
 const router = new Router()
 
@@ -21,10 +23,48 @@ async (req, res) => {
         if(candidate){
             return res.status(400).json({message: `User with email ${email} already exist`})
         }
-        const hashPassword = await bcrypt.hash(password, 15)
+        const hashPassword = await bcrypt.hash(password, 8)
         const user = new User({email, password: hashPassword})
         await user.save()
         return res.json({message: "User was created"})
+
+    }catch (e) {
+        console.log(e)
+        res.send({message: "Server error"})
+    }
+})
+
+module.exports = router
+
+
+
+router.post('/login',
+[
+    check('email', "Incorrect email").isEmail(),
+    check('password', "Password must be longer than 3 and shorter then 12").isLength({min:3, max:12}) 
+],
+async (req, res) => {
+    try{
+      const { email, password } = req.body
+      const user = await User.findOne({email})
+      if(!user){
+          return res.status(404).json({message: "User not found"})
+      }
+      const isPassValid = bcrypt.compareSync(password, user.password)
+      if(!isPassValid){
+          return res.status(400).json({message: "Invalid password"})
+      }
+      const token = jwt.sign({id: user.id}, config.get("secret"), { expiresIn: "1h"})
+      return res.json({
+          token,
+          user: {
+              id: user.id,
+              email: user.email,
+              diskSpace: user.diskSpace,
+              usedSpace: user.usedSpace,
+              avatar: user.avatar
+          }
+      })
 
     }catch (e) {
         console.log(e)
