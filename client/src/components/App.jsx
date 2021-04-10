4838abcea3fc975789a4a35add63cb14982c7983
Evgeny <starchenkogin@gmail.com>
2021-04-10 22:26:43 +0300

    jwt token

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './navbar/Navbar'
 import './app.css'
 import { BrowserRouter, Route,  Switch } from "react-router-dom"
 import Registration from "./authorization/Registration"
 import Login from './authorization/Login'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import  { auth } from  '../actions/user'
 
 
 function App() {
    const isAuth = useSelector( state => state.user.isAuth )
+   const dispatch = useDispatch()
+
+   useEffect(() => { dispatch(auth()) }, [dispatch])
 
   return (
     <BrowserRouter>
