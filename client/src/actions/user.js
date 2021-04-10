ed312e6a5a1d028afa577dbe45275728918e0b74
Evgeny <starchenkogin@gmail.com>
2021-04-10 13:27:24 +0300

    login

--- client/src/actions/user.js
+++ client/src/actions/user.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { setUser }from '../reducers/userReducer'
 
 export const registration = async (email, password) => {
     try {
@@ -11,4 +12,21 @@ export const registration = async (email, password) => {
         alert(e.response.data.message)
     }
 
+}
+
+export const login =  (email, password) => {
+    return async dispatch => {
+        try {
+            const response = await axios.post(`http://localhost:5000/api/auth/login`, {
+                email,
+                password
+            })
+            dispatch(setUser(response.data.user))
+            localStorage.setItem('token', response.data.token)
+            console.log(response)
+        } catch (e) {
+            alert(e.response.data.message)
+        }
+
+    }
 }
--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,18 +2,26 @@ import React from 'react'
 import Navbar from './navbar/Navbar'
 import './app.css'
 import { BrowserRouter, Route,  Switch } from "react-router-dom"
-import Registration from "./registration/Registration"
+import Registration from "./authorization/Registration"
+import Login from './authorization/Login'
+import { useSelector } from 'react-redux'
 
 
 function App() {
+   const isAuth = useSelector( state => state.user.isAuth )
+
   return (
     <BrowserRouter>
         <div className='app'>
            <Navbar />
            <div className="warp">
-           <Switch>
-             <Route path="/registration" component={Registration} />
-           </Switch>
+             {!isAuth && 
+                      <Switch>
+                      <Route path="/registration" component={Registration} />
+                      <Route path="/login" component={Login} />
+                    </Switch>
+             }
+  
            </div>
        </div>
     </BrowserRouter>
