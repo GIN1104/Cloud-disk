d3e6b541fd259ccf0697734112ec0933d39a47df
Evgeny Starchenko <starchenkogin@gmail.com>
2021-04-26 13:09:16 +0300

    add delete files

--- client/src/components/disk/fileList/file/FIle.jsx
+++ client/src/components/disk/fileList/file/FIle.jsx
@@ -4,7 +4,7 @@ import dirLogo from '../../../../assets/img/dir.svg'
 import fileLogo from '../../../../assets/img/file.svg'
 import { useDispatch, useSelector } from "react-redux"
 import { pushToStack, setCurrentDir } from "../../../../reducers/fileReducer"
-import { downloadFile } from '../../../../actions/file';
+import { deleteFile, downloadFile } from '../../../../actions/file';
 
 const File = ({file}) => {
     const dispatch = useDispatch()
@@ -19,6 +19,10 @@ function downloadClickHandler(e) {
     e.stopPropagation()
     downloadFile(file)
 }
+function deleteClickHandler(e){
+    e.stopPropagation()
+    dispatch(deleteFile(file))
+}
     return (
         <div className='file' onClick={() => openDirHandler() }>
             <img src={file.type === 'dir' ? dirLogo : fileLogo} alt="" className="file__img"/>
@@ -26,7 +30,7 @@ function downloadClickHandler(e) {
             <div className="file__date">{file.date.slice(0,10)}</div>
             <div className="file__size">{file.size}</div>
       { file.type !== "dir"  &&   <button onClick={(e) => downloadClickHandler(e)} className="file__btn file__download">Download</button>}
-            <button className="file__btn file__delete">Delete</button>
+            <button onClick={(e) => deleteClickHandler(e)} className="file__btn file__delete">Delete</button>
         </div>
     );
 };
